89065d8a45dd1b2ea02bfeb85d92dcdfffe3bd27
BangHann <hibatullahfarhan4@gmail.com>
2022-10-23 11:48:57 +0700

    login

--- login-page.js
+++ login-page.js
@@ -1,16 +1,48 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+let baseURL = 'https://63497f50a59874146b2192cc.mockapi.io/ayf/'
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+const emailElement = document.querySelector('#exampleInputEmail1')
+const passwordElement = document.querySelector('#exampleInputPassword1')
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
+let login = async (email, password) => {
+    
+    let response = await fetch(baseURL + 'users', {
+        method: 'GET'
+    })
+
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
     }
+
+    let data = await response.json()
+    const user = await data.find(d => d.email === email)
+
+    if(user){
+        if (user.password === password) {
+            localStorage.setItem("id", user.id)
+            localStorage.setItem("full_name", user.full_name)
+            // headerElement.innerHTML = "Login Success"
+            window.location.href = "dashboard-page.html"
+        } else {
+            headerElement.innerHTML = "Wrong Password"
+        }
+    } else {
+        headerElement.innerHTML = "User Not Found"
+    }
+
+    buttonLogin.innerHTML = "Login"
+    emailElement.value = ""
+    passwordElement.value = ""
+}
+
+buttonLogin.addEventListener('click', (event) => {
+    event.preventDefault()
+    
+    buttonLogin.innerHTML = `
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    `
+    
+    const email = emailElement.value
+    const password = passwordElement.value
+
+    login(email, password)
 })
