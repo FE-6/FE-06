aeb769d02c06ab6798261e5e8f693545a4a390e8
BangHann <hibatullahfarhan4@gmail.com>
2022-10-23 13:10:39 +0700

    login

--- login-page.js
+++ login-page.js
@@ -1,7 +1,9 @@
-let baseURL = 'https://63497f50a59874146b2192cc.mockapi.io/ayf/'
+let baseURL = 'https://6353739ca9f3f34c3752aeb7.mockapi.io/api/fe6/login'
 
 const emailElement = document.querySelector('#exampleInputEmail1')
 const passwordElement = document.querySelector('#exampleInputPassword1')
+const buttonLogin = document.getElementById('button')
+
 
 let login = async (email, password) => {
     
